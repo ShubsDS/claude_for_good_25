e83c1cdfdc1c3780a518457666024c4225507be3
ShubsDS <shrubsdaone@gmail.com>
2025-11-15 13:40:31 -0500

    fixes

--- view_data.py
+++ view_data.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Get grading by ID
-response = requests.get('http://localhost:8000/gradings/1')
+response = requests.get('http://localhost:8000/gradings/2')
 grading = response.json()
 
 # Pretty print the results
